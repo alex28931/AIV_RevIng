47bac45ebdf639ce94dfbb768cbef70951905e28
alex28931 <alex28931@gmail.com>
2025-05-12 17:03:33 +0200

    SandsOfTimeMechanic

---
+++ Source/AIV_RevIng/Private/TimeRewinder.cpp
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimeRewinder.h"
+
+// Sets default values for this component's properties
+UTimeRewinder::UTimeRewinder()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	MaxSnaphots = 50;
+	RewindSpeed = 3.0f;
+}
+
+void UTimeRewinder::RegisterSnapshot()
+{
+	AActor* Owner = GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+	FVector CurrentPosition = Owner->GetActorLocation();
+	UWorld* CurrentWorld = GetWorld();
+	if (!CurrentWorld)
+	{
+		return;
+	}
+	double TimeStamp = CurrentWorld->GetTimeSeconds();
+	SnapShots.EmplaceLast(TimeStamp, CurrentPosition);
+	if (SnapShots.Num() >= MaxSnaphots) 
+	{
+		SnapShots.PopFirst();
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Time: %f NumSnapShot: %d"), TimeStamp, SnapShots.Num());
+	DrawDebugSphere(CurrentWorld, SnapShots.Last().Position, 5.0f, 32, FColor::Red, false, 2.0f);
+}
+
+void UTimeRewinder::RewindPosition(float DeltaTime)
+{
+	RewindAccumulator += RewindSpeed * DeltaTime;
+	FVector PrevPosition = SnapShots.Last().Position;
+	AActor* Owner = GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+	Owner->SetActorLocation(FMath::Lerp(StartRewindPosition, PrevPosition, RewindAccumulator));
+	if (RewindAccumulator >= 1) 
+	{
+		Owner->SetActorLocation(PrevPosition);
+		StartRewindPosition = PrevPosition;
+		SnapShots.PopLast();
+		RewindAccumulator = 0.0f;
+	}
+}
+
+void UTimeRewinder::BeginRewindPosition()
+{
+	AActor* Owner = GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+	StartRewindPosition = Owner->GetActorLocation();
+	UWorld* CurrentWorld = GetWorld();
+	if (!CurrentWorld)
+	{
+		return;
+	}
+	StartRewindTimeStamp= CurrentWorld->GetTimeSeconds();
+}
+
+bool UTimeRewinder::CanRewind()
+{
+	return !SnapShots.IsEmpty();
+}
+
+
+// Called when the game starts
+void UTimeRewinder::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UTimeRewinder::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/AIV_RevIng/Public/TimeRewinder.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Containers/Deque.h"
+#include "TimeRewinder.generated.h"
+
+struct FSnapShot
+{
+	double TimeSTamp;
+	FVector Position;
+
+	FSnapShot(double InTimeStamp,FVector InPosition) : TimeSTamp(InTimeStamp), Position(InPosition)
+	{}
+};
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class AIV_REVING_API UTimeRewinder : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTimeRewinder();
+	UFUNCTION(BlueprintCallable)
+	void RegisterSnapshot();
+	UFUNCTION(BlueprintCallable)
+	void RewindPosition(float DeltaTime);
+	UFUNCTION(BlueprintCallable)
+	void BeginRewindPosition();
+	UFUNCTION(BlueprintCallable)
+	bool CanRewind();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 MaxSnaphots;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float RewindSpeed;
+	FVector StartRewindPosition;
+	double StartRewindTimeStamp;
+	float RewindAccumulator;
+
+private:
+	TDeque<FSnapShot> SnapShots;
+};
